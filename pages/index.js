86028548cd1d4c75c5df7a7017d8498be1d41d52
Vishhvak Srinivasan <vishhvak@cmu.edu>
2022-03-15 22:43:22 -0400

    Added request for fetching movies from milestone deployment

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,25 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
+  var XMLHttpRequest = require('xhr2');
+  const xhr = new XMLHttpRequest()
+  //open a get request with the remote server URL
+  xhr.open("GET", "http://localhost:8010/proxy/recommend/231204")
+  //send the Http request
+  xhr.send()
+
+  //EVENT HANDLERS
+
+  //triggered when the response is completed
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      //parse JSON datax`x
+      var data = xhr.responseText
+      console.log(data);
+    } else if (xhr.status === 404) {
+      console.log("No records found")
+    }
+  }
   return (
     <div className="container">
       <Head>
