00d075336955ab26eb71f07bb8b766c11c9a0fe5
Andrew Lyubovsky <a.l.andlyu@gmail.com>
2022-03-15 22:10:54 -0400

    added fetch

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
+
+  fetch('http://128.2.205.103:8082/recommend/231204')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
   return (
     <div className="container">
       <Head>
